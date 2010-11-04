98d812e9f59214adc830494a77bcaff8678f0ce8
Fred Wu <canglan@gmail.com>
2010-11-04 16:27:24 +1100

    Remove trailing spaces.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ActiveRecord::Base
   include Datamappify::Resource
-  
+
   EMAIL_REGEX = /^[A-Z0-9._%-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}$/i
-  
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
-  
+
   # Setup accessible (or protected) attributes for your model
   # attr_accessible :email, :first_name, :last_name, :payment_email, :password, :password_confirmation
-  
+
   property  :email, :string, :default => "", :null => false
   property  :first_name, :string, :limit => 50
   property  :last_name, :string, :limit => 50
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
   property  :confirmed_at, :datetime
   property  :confirmation_sent_at, :datetime
   property  :timestamps
-  
+
   add_index :email, :unique => true
   add_index :confirmation_token, :unique => true
   add_index :reset_password_token, :unique => true
