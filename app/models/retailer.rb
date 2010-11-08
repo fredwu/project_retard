fc6283a2f0258ac2e060c43791216cb1967570b4
Fred Wu <canglan@gmail.com>
2010-11-08 22:55:19 +1100

    Allow underscores in product and retailer code.

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -13,7 +13,7 @@ class Retailer < ActiveRecord::Base
   validates_presence_of   :code
   validates_uniqueness_of :name
   validates_uniqueness_of :code
-  validates_format_of     :code, :with => /^[a-zA-Z0-9]+$/, :message => "can only contain alphanumeric characters"
+  validates_format_of     :code, :with => /^[a-zA-Z0-9_]+$/, :message => "can only contain alphanumeric and underscore characters"
 
   has_many :products
   has_many :users
