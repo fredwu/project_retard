cd8505335e696d3b622987b7ff5858c909bcaee8
Fred Wu <canglan@gmail.com>
2010-11-12 19:01:16 +1100

    Split application.js into application/admin/public. Updated and added slideshow for product images.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -30,34 +30,6 @@ $(document).ready(function() {
   });
 
   /* --------------------
-    Fancybox
-  -------------------- */
-
-  $("a.fancybox").fancybox({
-    speedOut: 0
-  });
-
-  /* --------------------
-    Styling - they're here to 'automagically' fix the stock Adminus theme
-  -------------------- */
-
-  if ($("label").length > 0) {
-    $("label").before(" ");
-  }
-  $("label.collection_radio").css("font-weight", "normal");
-  $("input.string, input.password, input.numeric").addClass("text small");
-  $("form p > label").after("<br />");
-  $("#error_explanation").addClass("message errormsg");
-  $("p.field_with_errors > span.error").addClass("note");
-  $(".nohide").children(".close").hide();
-  $("input.file").parent("div").css("background-image", "url(/adminus/original/images/upload.gif)");
-  $(".pagination").addClass("paggination right");
-  $(".pagination em").wrap("<a class='active'>").css("font-style", "normal");
-  $(".pagination span.disabled").hide();
-  $(".pagination a.previous_page").text("«");
-  $(".pagination a.next_page").text("»");
-
-  /* --------------------
     Resource removals
   -------------------- */
 
@@ -82,27 +54,4 @@ $(document).ready(function() {
     }
   });
 
-  $("input:disabled").css({color: "#999"});
-
-  /* --------------------
-    Product publish toggle
-  -------------------- */
-
-  $("input#product_is_activated").change(function() {
-    show_publish_block($(this));
-  });
-
-  if ($("input#product_is_activated").length >= 0) {
-    show_publish_block($("input#product_is_activated"));
-  }
-
-  function show_publish_block(scope) {
-    if ($(scope).attr("checked")) {
-      $("#start_at p.datetime, #end_at p.datetime").fadeIn();
-      $("#start_at p.datetime select, #end_at p.datetime select").attr("disabled", false);
-    } else {
-      $("#start_at p.datetime, #end_at p.datetime").hide();
-      $("#start_at p.datetime select, #end_at p.datetime select").attr("disabled", true);
-    }
-  }
 });
---
+++ public/javascripts/public.js
@@ -0,0 +1,18 @@
+/*
+  Author: Fred Wu (http://fredwu.me/)
+*/
+
+$(document).ready(function() {
+
+  /* --------------------
+    Product images
+  -------------------- */
+
+  $("#deal #images").slideshow({
+    width: 620,
+    height: 240,
+    pauseSeconds: 5,
+    caption: false
+  });
+
+});
